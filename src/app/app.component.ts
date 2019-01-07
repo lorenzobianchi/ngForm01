a78eb99e9967d26dea73f094220abd4717127d23
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2019-01-07 21:13:05 +0100

    added clock in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  click$ = new Subject();
   title = 'ngForm01';
+  clock = interval(1000).pipe(
+    map(() => {
+      return new Date();
+    })
+  );
+  clock2$;
+
+  constructor() {
+    this.clock.subscribe(console.log.bind(console));
+    this.clock2$ = this.click$.pipe(
+      map(
+        () => new Date()
+      )
+    );
+  }
+  
 }
